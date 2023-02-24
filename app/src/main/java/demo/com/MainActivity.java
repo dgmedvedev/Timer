fb85ad293a6fb18df1f6db3c9ce684964f7988cc
dgmedvedev <dg-medvedev@mail.ru>
2023-02-24 17:30:07 +0300

    add methods onPause(),onResume()

--- app/src/main/java/demo/com/MainActivity.java
+++ app/src/main/java/demo/com/MainActivity.java
@@ -44,15 +44,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = isRunning;
         isRunning = false;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         isRunning = wasRunning;
     }
 
